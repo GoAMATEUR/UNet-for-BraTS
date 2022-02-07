6e762158f3d56ca71fd192a1580df121c93c4083
GOAMATEUR <hsy20010522@hotmail.com>
2022-02-07 23:07:49 +0800

    add MetricsTracker

--- Train.py
+++ Train.py
@@ -1,17 +1,15 @@
 """
     By:     Hsy
-    Update: 2022/2/4
+    Update: 2022/2/7
 """
 import os
 import argparse
 import numpy as np
-from tensorboard import summary
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.tensorboard import SummaryWriter
-from torchvision.transforms import ToPILImage
 from net.unet import UNet
 from utils.dataloader import BraTSDataset
 from utils.loss import BinaryDiceLoss, MetricsTracker
@@ -19,15 +17,15 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # parse args
 parser = argparse.ArgumentParser()
-parser.add_argument("--tag", type=str, help="Tag of this training session")
-parser.add_argument("--backbone", type=str, help="type of backbone net, VGG16 or ResNet50", default="VGG16")
-parser.add_argument("--batch_size", type=int, help="batch size", default=8)
-parser.add_argument("--summary_writer", type=int, default=0)
-parser.add_argument("--data_root", type=str, help="root of train set", default=".\\dataset")
-parser.add_argument("--model_root", type=str, help="root of train set", default=".\\out_models")
-parser.add_argument("--log_root", type=str, help="root of train set", default=".\\logs")
-parser.add_argument("--model_path", type=str, help="(Optional) Path of pretrained model", default=None)
-parser.add_argument("--lr", type=float, help="initial learning rate", default=1e-3)
+parser.add_argument("--tag",            type=str,   help="Tag of this training session")
+parser.add_argument("--backbone",       type=str,   help="type of backbone net, VGG16 or ResNet50",     default="VGG16")
+parser.add_argument("--batch_size",     type=int,   help="batch size",                                  default=8)
+parser.add_argument("--summary_writer", type=int,   help="use summary writer as log",                   default=0)
+parser.add_argument("--data_root",      type=str,   help="root of train set",                           default=".\\dataset")
+parser.add_argument("--model_root",     type=str,   help="root of train set",                           default=".\\out_models")
+parser.add_argument("--log_root",       type=str,   help="root of train set",                           default=".\\logs")
+parser.add_argument("--model_path",     type=str,   help="(Optional) Path of pretrained model",         default=None)
+parser.add_argument("--lr",             type=float, help="initial learning rate",                       default=1e-3)
 args = parser.parse_args()
 
 train_tag = args.tag
@@ -43,7 +41,8 @@ if summary_writer == 1:
     writer = SummaryWriter(log_root)
 if not os.path.exists(model_root):
     os.makedirs(model_root)
-
+if not os.path.exists(log_root):
+    os.makedirs(log_root)
 # load trainset
 dataset = BraTSDataset(data_root)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -75,9 +74,12 @@ scheduler = ReduceLROnPlateau(opt, mode="min", patience=4, verbose=True)
 loss_bce = nn.BCELoss()
 loss_dice = BinaryDiceLoss()
 
-metrics = MetricsTracker()
+# keep track of metrics
+metrics = MetricsTracker(train_tag, log_root)
 
 epoch = 1
+weight_bce = 0.5
+weight_dice = 0.5
 while True:
     
     metrics.set_epoch(epoch)
@@ -89,11 +91,14 @@ while True:
         img, label = img.to(device), label.to(device)
         out_image = net(img)
         
-        iter_loss = loss_bce(out_image, label)
-        
+        iter_bce_loss = loss_bce(out_image, label)
         
         one_hot = (out_image>0.5).float()
-        iter_loss += loss_dice(one_hot, label)
+        
+        # dice loss + Binary Cross-Entropy loss
+        iter_dice_loss = loss_dice(one_hot, label)
+        
+        iter_loss = weight_bce * iter_bce_loss + weight_dice * iter_dice_loss
         iter_loss.requires_grad_(True)
         
         opt.zero_grad()
@@ -103,30 +108,35 @@ while True:
         # cpu_img = img.detach().cpu()
         # cpu_out = out_image.detach().cpu()
         
-        if i % 100 == 0:
-            print("--Epoch: {}, iter: {}, iter_loss: {}".format(epoch, i, iter_loss.item()))
+        # if i % 100 == 0:
+        #     print("--Epoch: {}, iter: {}, iter_loss: {}".format(epoch, i, iter_loss.item()))
+        
         with torch.no_grad():
             loss_sum += iter_loss.item()
             iter_count += 1
             if iter_loss.item() < min_loss:
                 min_loss = iter_loss.item()
+            metrics.update(out_image, label, iter_dice_loss.item())
             # update metrics
-    avg_loss = loss_sum / iter_count
-    print("Epoch {} | avg_loss:{} | min_loss: {}".format(epoch, avg_loss, min_loss))
+    val_loss = loss_sum / iter_count
+    acc, iou, dice = metrics.get_metrics()
     
+    print("Epoch {} | val_loss:{} | min_loss:{} | Acc:{} | IoU:{} | Dice:{} ".format(epoch, val_loss, min_loss, acc, iou, dice))
     # save state-dict
-    if ((epoch-1) % 5 == 0):
+    if ((epoch - 1) % 5 == 0):
         torch.save(net.state_dict(), os.path.join(model_root, "{}_{}.pth".format(train_tag, epoch)))
-    
+        metrics.save_logs()
+        
     # tensorboard
     if summary_writer == 1:
         epoch_sample = [img[:,0,:,:].reshape(-1, 1, H, W), label, out_image]
         tags = ['img', 'label', 'out']
         for i in range(3):
             writer.add_images("epoch {}".format(epoch), epoch_sample[i])
-        writer.add_scalar("{} Loss".format(train_tag), avg_loss, epoch)
+        writer.add_scalar("{} Loss".format(train_tag), val_loss, epoch)
         writer.close()
     
+    scheduler.step(val_loss)
     epoch += 1
     torch.cuda.empty_cache()
     
--- utils/loss.py
+++ utils/loss.py
@@ -1,46 +1,77 @@
 """
     By:     hsy
-    Update: 2022/2/4
+    Update: 2022/2/7
 """
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
-
+import json
+import os
 
 class MetricsTracker(object):
-    def __init__(self, tag=None):
+    def __init__(self, tag, log_root):
         self.iter_IoUs = []
         self.iter_Dices = []
         self.iter_Accs = []
         self.epoch = 0
         self.tag = tag
+        self.log_root = log_root
+        self.epoch_IoUs = []
+        self.epoch_Dices = []
+        self.epoch_Accs = []
     
     def update(self, input: torch.Tensor, target: torch.Tensor, dice):
         self.iter_IoUs.append(self.IoU(input, target))
-        self.iter_Dices.append(self.Acc(input, target))
+        self.iter_Accs.append(self.Acc(input, target))
+        self.iter_Dices.append(dice)
     
     def get_metrics(self):
         iou = np.mean(self.iter_IoUs)
         dice = np.mean(self.iter_Dices)
         acc = np.mean(self.iter_Accs)
+        self.epoch_IoUs.append(iou)
+        self.epoch_Dices.append(dice)
+        self.epoch_Accs.append(acc)
         return acc, iou, dice
         
     def save_logs(self):
-        return 
-    
+        dict = {"tag":self.tag, "epoch": self.epoch, "eval":{"iou":self.epoch_IoUs, "acc": self.epoch_Accs, "dice":self.epoch_Dices}}
+        item = json.dumps(dict)
+        with open(os.path.join(self.log_root, f"{self.tag}_{self.epoch}_eval.json"), "w", encoding="utf-8") as f:
+            f.write(item)
+        print(f"Eval log {self.tag}_{self.epoch}_eval.json written")
+        
     def set_epoch(self, epoch):
         self.epoch = epoch
+        
         self.iter_Dices = []
-        self.iter_Accs = []
-        self.iter_IoUs = []
-      
+        self.iter_Accs  = []
+        self.iter_IoUs  = []
+    
     def IoU(self, input: torch.Tensor, target: torch.Tensor)->float:
-        return
+        N = target.size()[0]
+        target = target.view(N, -1)
+        input = input.view(N, -1)
+        intersection = target*input
+        intersection = intersection.sum(1)
+        target_sum = target.sum(1)
+        input_sum = input.sum(1)
+        
+        ious = intersection / (target_sum + input_sum - intersection)
+        iou = ious.mean()
+        return float(iou)
 
     def Acc(self, input: torch.Tensor, target: torch.Tensor)->float:
-        return
+        N, C, H, W = target.size()
+        target = target.view(N, -1)
+        input = input.view(N, -1)
+    
+        
+        intersection = (target == input).float()
+        
+        return float(intersection.sum() / (N * H * W))
 
 class BinaryDiceLoss(nn.Module):
     """Simple implementation of Binary Dice Loss
@@ -73,67 +104,82 @@ class BinaryDiceLoss(nn.Module):
         loss = 1 - N_dice_eff.sum() / N
         return loss
 
-        
-        
-        
- 
+# class FocalLoss(nn.Module):
+#     def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
+#         super(FocalLoss, self).__init__()
+#         if alpha is None:
+#             self.alpha = Variable(torch.ones(class_num, 1))
+#         else:
+#             if isinstance(alpha, Variable):
+#                 self.alpha = alpha
+#             else:
+#                 self.alpha = Variable(alpha)
+#         self.gamma = gamma
+#         self.class_num = class_num
+#         self.size_average = size_average
+
+#     def forward(self, inputs, targets):
+#         N = inputs.size(0)
+#         C = inputs.size(1)
+#         P = F.softmax(inputs)
+
+#         class_mask = inputs.data.new(N, C).fill_(0)
+#         class_mask = Variable(class_mask)
+#         ids = targets.view(-1, 1)
+#         class_mask.scatter_(1, ids.data, 1.)
+#         #print(class_mask)
 
 
+#         if inputs.is_cuda and not self.alpha.is_cuda:
+#             self.alpha = self.alpha.cuda()
+#         alpha = self.alpha[ids.data.view(-1)]
 
-class FocalLoss(nn.Module):
-    def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
-        super(FocalLoss, self).__init__()
-        if alpha is None:
-            self.alpha = Variable(torch.ones(class_num, 1))
-        else:
-            if isinstance(alpha, Variable):
-                self.alpha = alpha
-            else:
-                self.alpha = Variable(alpha)
-        self.gamma = gamma
-        self.class_num = class_num
-        self.size_average = size_average
+#         probs = (P*class_mask).sum(1).view(-1,1)
 
-    def forward(self, inputs, targets):
-        N = inputs.size(0)
-        C = inputs.size(1)
-        P = F.softmax(inputs)
+#         log_p = probs.log()
+#         #print('probs size= {}'.format(probs.size()))
+#         #print(probs)
 
-        class_mask = inputs.data.new(N, C).fill_(0)
-        class_mask = Variable(class_mask)
-        ids = targets.view(-1, 1)
-        class_mask.scatter_(1, ids.data, 1.)
-        #print(class_mask)
+#         batch_loss = -alpha*(torch.pow((1-probs), self.gamma))*log_p 
+#         #print('-----bacth_loss------')
+#         #print(batch_loss)
 
 
-        if inputs.is_cuda and not self.alpha.is_cuda:
-            self.alpha = self.alpha.cuda()
-        alpha = self.alpha[ids.data.view(-1)]
-
-        probs = (P*class_mask).sum(1).view(-1,1)
-
-        log_p = probs.log()
-        #print('probs size= {}'.format(probs.size()))
-        #print(probs)
-
-        batch_loss = -alpha*(torch.pow((1-probs), self.gamma))*log_p 
-        #print('-----bacth_loss------')
-        #print(batch_loss)
-
-
-        if self.size_average:
-            loss = batch_loss.mean()
-        else:
-            loss = batch_loss.sum()
-        return loss
+#         if self.size_average:
+#             loss = batch_loss.mean()
+#         else:
+#             loss = batch_loss.sum()
+#         return loss
     
 if __name__ == "__main__":
-    loss_dice = BinaryDiceLoss()
-    target = torch.tensor([[[[1, 0],
-                            [0, 1]]],
-                           [[[1, 0],
-                            [0, 1]]]])
-    input = torch.tensor([[[[1, 1],
-                            [0, 1]]],
-                            [[[1, 1],
-                            [0, 1]]]])
+    with open(".\\logs\\dice_test\\dice_test_1_eval.json", "w", encoding="utf-8") as f:
+        f.write("test")
+    # loss_dice = BinaryDiceLoss()
+    # target = torch.tensor([[[[1., 1.],
+    #                         [0., 1.]]],
+    #                        [[[1., 1.],
+    #                         [1., 1.]]],
+    #                        [[[1., 1.],
+    #                         [1., 1.]]]])
+    # input = torch.tensor([[[[0., 1.],
+    #                         [0., 0.]]],
+    #                         [[[1., 0.],
+    #                         [0., 1.]]],
+    #                         [[[1., 1.],
+    #                         [1., 1.]]]])
+    
+    # N, C, H, W = target.size()
+    # target = target.view(N, -1)
+    # input = input.view(N, -1)
+   
+    
+    # intersection = (target == input).float()
+    # print(float(intersection.sum() / (N * H * W)))
+    # # intersection = intersection.sum(1)
+    # # target_sum = target.sum(1)
+    # # input_sum = input.sum(1)
+    
+    # # ious = intersection / (target_sum + input_sum - intersection)
+    # # iou = float(ious.mean())
+    # # print(iou)
+    
